c9c5764cb61132157d17f59760320dc5084bedb6
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-06-01 12:51:11 +0300

    added avgtool

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,4 +1,10 @@
 echo "Downloading Strings"
 unzip loco.zip
-
 ./loco/loco.rb
+
+
+if [ "$CI_WORKFLOW" = "Firebase Debug Build" ];
+then
+	echo "Updating build version"
+	agvtool new-version -all 999999
+fi
