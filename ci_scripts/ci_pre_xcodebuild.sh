07e6070c2f4d22e4310acf75b5202173241307ee
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-06-02 11:17:46 +0300

    updates

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -1,11 +1,3 @@
 echo "Downloading Strings"
 unzip loco.zip
 ./loco/loco.rb
-
-
-if [ "$CI_WORKFLOW" = "Firebase Debug Build" ];
-then
-	echo "Updating build version"
-	cd ..
-	agvtool new-version -all 999999
-fi
