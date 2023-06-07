469d970da6664a0c3667e4e872f251395ece7af3
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-06-07 19:02:15 +0300

    added fabric config

--- testXCloud/ContentView.swift
+++ testXCloud/ContentView.swift
@@ -14,37 +14,8 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-            Text(setEnvironmentVars())
-            List {
-                ForEach(getEnv(), id: \.self) { section in
-                    Text(section)
-                }
-            }
         }
         .padding()
-    }
-    
-    private func setEnvironmentVars() -> String {
-        if let releaseBuild = ProcessInfo.processInfo.environment["RELEASE_BUILD"] {
-            return releaseBuild
-        } else {
-            #if DEBUG
-                return "fallback debug"
-            #elseif FABRIC
-                return "fallback Debug fabric"
-            #else
-                return "fallback Debug default"
-            #endif
-        }
-    }
-    
-    func getEnv() -> [String] {
-        var envVAR = [String]()
-        for (key, value) in ProcessInfo.processInfo.environment {
-            print("\(key): \(value)")
-            envVAR.append(key)
-        }
-        return envVAR
     }
 }
 
