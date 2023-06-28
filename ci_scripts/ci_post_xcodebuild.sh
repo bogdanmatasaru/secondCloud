ece308e38b6c55214cc0bdf566e8e4d480dc0306
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-06-28 12:17:42 +0300

    up

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,9 +1,7 @@
 if [[ $CI_WORKFLOW == *"Firebase"* ]]; then
     echo "Uploading Firebase Debug Build"
     cd $CI_AD_HOC_SIGNED_APP_PATH
-    for [ z in `echo *` ]; do
-        echo "$z"
-    done
+    ls
     unzip firebase-tools-macos.zip
     ./firebase-tools-macos appdistribution:distribute $CI_AD_HOC_SIGNED_APP_PATH/$CI_PRODUCT.ipa --app "$FIREBASE_APPID" --token "$FIREBASE_TOKEN" --groups "ios-testers" --release-notes "BRANCH: ${CI_BRANCH} \n\nRelease Notes:\n $(git log -1 --pretty=%B) \n ${CI_GIT_REF}"
 fi
