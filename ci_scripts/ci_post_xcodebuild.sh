19c3e1c005b6937133b140da5e029516dad6237f
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-06-29 10:41:39 +0300

    updated RL

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -4,3 +4,8 @@ if [[ $CI_WORKFLOW == *"Firebase"* ]]; then
     unzip firebase-tools-macos.zip
     ./firebase-tools-macos appdistribution:distribute $CI_AD_HOC_SIGNED_APP_PATH/$CI_PRODUCT.ipa --app "$FIREBASE_APPID" --token "$FIREBASE_TOKEN" --groups "ios-testers" --release-notes "BRANCH: ${CI_BRANCH} \n\nRelease Notes:\n $(git log -1 --pretty=%B) \n ${CI_GIT_REF}"
 fi
+
+echo "Creating release notes"
+TESTFLIGHT_DIR_PATH=../TestFlight
+mkdir $TESTFLIGHT_DIR_PATH
+echo "BRANCH: ${CI_BRANCH} \n\nRelease Notes:\n $(git log -3 --pretty=%B) \n ${CI_GIT_REF}" > $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -9,8 +9,3 @@ if [[ $CI_WORKFLOW == *"Firebase"* ]]; then
 else
 	plutil -replace backendSettingsEnabled -bool NO ../eMAG/SupportingFiles/Shared/Environment/Environment.plist
 fi
-
-echo "Creating release notes"
-TESTFLIGHT_DIR_PATH=../TestFlight
-mkdir $TESTFLIGHT_DIR_PATH
-echo "BRANCH: ${CI_BRANCH} \n\nRelease Notes:\n $(git log -3 --pretty=%B) \n ${CI_GIT_REF}" > $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
