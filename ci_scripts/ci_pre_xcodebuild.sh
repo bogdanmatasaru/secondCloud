27c27ab62ae5af6f377e5f7f20095f09aa0eacf8
bogdanmatasaru <bogdan.matasaru@yahoo.com>
2023-06-29 09:58:25 +0300

    added release notes sh

--- ci_scripts/ci_pre_xcodebuild.sh
+++ ci_scripts/ci_pre_xcodebuild.sh
@@ -9,3 +9,10 @@ if [[ $CI_WORKFLOW == *"Firebase"* ]]; then
 else
 	plutil -replace backendSettingsEnabled -bool NO ../eMAG/SupportingFiles/Shared/Environment/Environment.plist
 fi
+
+echo "Creating release notes"
+if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
+  TESTFLIGHT_DIR_PATH=../TestFlight
+  mkdir $TESTFLIGHT_DIR_PATH
+  git fetch --deepen 3 && git log -3 --pretty=format:"%s" >! $TESTFLIGHT_DIR_PATH/WhatToTest.en-US.txt
+fi
